fa5eb63658b7d645b51170c3dca8a2830a97efb1
Artur Sarakhman <Artur_Sarakhman@epam.com>
2022-02-16 20:09:26 +0200

    initial commit

---
+++ src/main/java/com/kpi/tefresolver/TefResolverApplication.java
@@ -0,0 +1,13 @@
+package com.kpi.tefresolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TefResolverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TefResolverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kpi/tefresolver/TefResolverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpi.tefresolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TefResolverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
