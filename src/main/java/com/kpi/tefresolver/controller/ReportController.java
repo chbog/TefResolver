b533ff63495978390f54421bef424dda070f87bf
Artur Sarakhman <Artur_Sarakhman@epam.com>
2022-02-17 20:12:39 +0200

    implement simple ReportController

---
+++ src/main/java/com/kpi/tefresolver/controller/ReportController.java
@@ -0,0 +1,39 @@
+package com.kpi.tefresolver.controller;
+
+import com.kpi.tefresolver.model.Report;
+import com.kpi.tefresolver.service.ReportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/reports")
+public class ReportController {
+    private ReportService reportService;
+
+    @GetMapping
+    public List<Report> all(){
+        return List.of(new Report()); //stub
+    }
+
+    @PostMapping
+    public String upload(@RequestParam("file") MultipartFile file){
+        return file.getContentType() + " " + file.getOriginalFilename(); //stub
+    }
+
+    @GetMapping("/{id}")
+    public Report one(@PathVariable Long id){
+        return new Report(); //stub
+    }
+
+    public ReportService getReportService() {
+        return reportService;
+    }
+
+    @Autowired
+    public void setReportService(ReportService reportService) {
+        this.reportService = reportService;
+    }
+}
