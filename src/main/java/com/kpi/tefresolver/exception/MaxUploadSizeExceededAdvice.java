8e49add9c503a2b215585370d984c28c20c8efa4
Artur Sarakhman <Artur_Sarakhman@epam.com>
2022-02-17 16:56:27 +0200

    minor improvements of Advices

--- src/main/java/com/kpi/tefresolver/exception/MaxUploadSizeExceededAdvice.java
+++ src/main/java/com/kpi/tefresolver/exception/MaxUploadSizeExceededAdvice.java
@@ -3,14 +3,16 @@ package com.kpi.tefresolver.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 @RestControllerAdvice
 public class MaxUploadSizeExceededAdvice {
     @ExceptionHandler(MaxUploadSizeExceededException.class)
-    public ResponseEntity<String> handleMaxSizeException(MaxUploadSizeExceededException e){
-        return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE).body("File too large!");
+    @ResponseStatus(HttpStatus.PAYLOAD_TOO_LARGE)
+    public String handleMaxSizeException(MaxUploadSizeExceededException e){
+        return "File too large!";
     }
 
 }
--- src/main/java/com/kpi/tefresolver/exception/ReportNotFoundAdvice.java
+++ src/main/java/com/kpi/tefresolver/exception/ReportNotFoundAdvice.java
@@ -3,12 +3,14 @@ package com.kpi.tefresolver.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ReportNotFoundAdvice {
     @ExceptionHandler(ReportNotFoundException.class)
-    public ResponseEntity<String> handleReportNotFoundException(ReportNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleReportNotFoundException(ReportNotFoundException e) {
+        return e.getMessage();
     }
 }
