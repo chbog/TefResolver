aa4a531ecf8cee0a256a14aab45626a3398e6ecc
Artur Sarakhman <Artur_Sarakhman@epam.com>
2022-02-21 02:46:07 +0200

    refactor excel to list of ObservationData conversion algorithm

--- src/main/java/com/kpi/tefresolver/util/ExcelUtil.java
+++ src/main/java/com/kpi/tefresolver/util/ExcelUtil.java
@@ -2,21 +2,13 @@ package com.kpi.tefresolver.util;
 
 import com.google.common.base.Strings;
 import com.kpi.tefresolver.model.ObservationData;
-import com.kpi.tefresolver.model.Report;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 @Component
@@ -27,38 +19,86 @@ public class ExcelUtil {
         return EXCEL_TYPE.equals(file.getContentType());
     }
 
-    public static Report excelToReport(MultipartFile file) {
+    public static List<ObservationData> excelToObservationData(MultipartFile file) {
         try {
             Workbook workbook = new XSSFWorkbook(file.getInputStream());
             Sheet sheet = workbook.getSheetAt(0);
-            Iterator<Row> rows = sheet.iterator();
             List<ObservationData> observationDataList = new ArrayList<>();
-            int rowNumber = 0;
-            while (rows.hasNext()) {
-                Row currentRow = rows.next();
+            boolean zeroRow = true;
+            for (Row currentRow : sheet) {
                 // skip header
-                if (rowNumber == 0) {
-                    rowNumber++;
-                    continue;
-                } else if (Strings.isNullOrEmpty(currentRow.getCell(0).getStringCellValue())) {
+                if (zeroRow) {
+                    zeroRow = false;
                     continue;
                 }
                 ObservationData observationData = new ObservationData();
-                int i = 0;
-                observationData.setDayNumber((short) currentRow.getCell(i++).getNumericCellValue());
-                String[] hoursAndMinutes = currentRow.getCell(i++).getStringCellValue().split(":");
-                observationData.setObservationTime(LocalTime.of(Integer.parseInt(hoursAndMinutes[0]), Integer.parseInt(hoursAndMinutes[1])));
-                observationData.setTemperature((short) currentRow.getCell(i++).getNumericCellValue());
-                observationData.setWindDirection(currentRow.getCell(i++).getStringCellValue());
-                observationData.setWindSpeed((int) currentRow.getCell(i).getNumericCellValue());
-
+                for (Cell currentCell : currentRow) {
+                    extractAndSetCellValue(observationData, currentCell);
+                }
                 observationDataList.add(observationData);
             }
             workbook.close();
             //needs to be validated first
-            return new Report(null, file.getOriginalFilename(), false, observationDataList);
+            return observationDataList;
         } catch (IOException e) {
             throw new RuntimeException("fail to parse Excel file: " + e.getMessage()); //needs to be replaced with custom exception and also needs overall refactoring
+        }
+    }
+
+    private static void extractAndSetCellValue(ObservationData data, Cell cell) {
+        try {
+            switch (cell.getColumnIndex()) {
+                case 0:
+                    extractAndSetDayNumber(data, cell);
+                    break;
+                case 1:
+                    extractAndSetObservationTime(data, cell);
+                    break;
+                case 2:
+                    extractAndSetTemperature(data, cell);
+                    break;
+                case 3:
+                    extractAndSetWindDirection(data, cell);
+                    break;
+                case 4:
+                    extractAndSetWindSpeed(data, cell);
+                    break;
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void extractAndSetDayNumber(ObservationData data, Cell cell) {
+        if (cell.getCellType() == CellType.NUMERIC) {
+            data.setDayNumber((short) cell.getNumericCellValue());
+        }
+    }
+
+    private static void extractAndSetObservationTime(ObservationData data, Cell cell) {
+        if (cell.getCellType() == CellType.NUMERIC) {
+            data.setObservationTime(cell.getLocalDateTimeCellValue().toLocalTime());
+        }
+    }
+
+    private static void extractAndSetTemperature(ObservationData data, Cell cell) {
+        if (cell.getCellType() == CellType.NUMERIC) {
+            data.setTemperature((short) cell.getNumericCellValue());
+        }
+    }
+
+    private static void extractAndSetWindDirection(ObservationData data, Cell cell) {
+        if (cell.getCellType() == CellType.STRING) {
+            String windDirection = cell.getStringCellValue();
+            if (!Strings.isNullOrEmpty(windDirection)) {
+                data.setWindDirection(windDirection);
+            }
+        }
+    }
+
+    private static void extractAndSetWindSpeed(ObservationData data, Cell cell) {
+        if (cell.getCellType() == CellType.NUMERIC) {
+            data.setWindSpeed((int) cell.getNumericCellValue());
         }
     }
 }
