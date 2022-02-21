0b62c16a0c9a60dd487110b02cb766167eca75a3
Artur Sarakhman <Artur_Sarakhman@epam.com>
2022-02-21 02:47:58 +0200

    implement excel file saving functionality

--- src/main/java/com/kpi/tefresolver/service/ReportService.java
+++ src/main/java/com/kpi/tefresolver/service/ReportService.java
@@ -1,5 +1,6 @@
 package com.kpi.tefresolver.service;
 
+import com.kpi.tefresolver.model.ObservationData;
 import com.kpi.tefresolver.model.Report;
 import com.kpi.tefresolver.repository.ReportRepository;
 import com.kpi.tefresolver.util.ExcelUtil;
@@ -7,17 +8,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.transaction.Transactional;
+import java.util.List;
+
 @Service
 public class ReportService {
     private ReportRepository reportRepository;
 
     public Report saveExcelFile(MultipartFile file){
         if(ExcelUtil.isExcelFormat(file)){
-            return ExcelUtil.excelToReport(file);
+            List<ObservationData> data = ExcelUtil.excelToObservationData(file);
+            //must be validated first
+            return saveReport(file.getOriginalFilename(), false, data);
         }
         throw new IllegalArgumentException("file is not of excel format");
     }
 
+    @Transactional
+    protected Report saveReport(String reportName, boolean valid, List<ObservationData> data){
+        Report report = reportRepository.save(new Report(null, reportName, valid, null));
+        data.forEach(row -> row.setReport(report));
+        report.setRows(data);
+        return reportRepository.save(report);
+    }
 
     public ReportRepository getReportRepository() {
         return reportRepository;
